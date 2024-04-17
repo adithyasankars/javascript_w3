3366b89b824bb3ad7e7c877bdf646fb54158a7a5
adithyasankars <167020861+adithyasankars@users.noreply.github.com>
2024-04-17 06:22:09 +0000

    JS outputs

---
+++ JS/02_output.js
@@ -0,0 +1,37 @@
+
+
+// -------------------------------------------------------- OUTPUT ----------------------------------------------------
+
+// JavaScript can "display" data in different ways:
+
+// 1. innerHTML
+
+
+document.getElementById("demo").innerHTML = 5 + 6;
+
+
+// // ---------------------------------------------------------------------------------------------------------------------------
+
+
+// // 2. document.write()
+
+document.write(5 + 6);  
+
+// mainly used for testing purpose
+// Using document.write() after an HTML document is loaded, will delete all existing HTML:
+
+// ---------------------------------------------------------------------------------------------------------------------------
+
+
+// 3. window.alert()
+
+
+window.alert(5 + 6);   // uses an alert box to display the content
+
+
+// ---------------------------------------------------------------------------------------------------------------------------
+
+// 4. console.log()
+
+console.log("My name is Adithya Sankar S");
+
