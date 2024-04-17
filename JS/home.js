5a2858d8dade01d7ceeb8629bae1a94c93fe241a
adithyasankars <167020861+adithyasankars@users.noreply.github.com>
2024-04-17 06:09:30 +0000

    JS home + linking to html - externally

---
+++ JS/home.js
@@ -0,0 +1,35 @@
+
+// --------------------------------------------------JAVASCRIPT-----------------------------------------------------------------
+
+// JavaScript is the world's most popular programming language.
+
+// JavaScript is the programming language of the Web.
+
+// --------------------------------------------Why Study JavaScript?------------------------------------------------------------
+
+// JavaScript is one of the 3 languages all web developers must learn:
+
+//    1. HTML to define the content of web pages
+
+//    2. CSS to specify the layout of web pages
+
+//    3. JavaScript to program the behavior of web pages
+
+// In HTML, JavaScript code is inserted between <script> and </script> tags.
+
+{/* <script>
+
+    document.getElementById("demo").innerHTML = "My First JavaScript";
+
+</script> */}
+ 
+//  ---------------------------------------------  External JavaScript Advantages-----------------------------------------------
+
+<script src="myScript.js"></script>
+
+// Placing scripts in external files has some advantages:
+
+// It separates HTML and code
+// It makes HTML and JavaScript easier to read and maintain
+// Cached JavaScript files can speed up page loads
+
