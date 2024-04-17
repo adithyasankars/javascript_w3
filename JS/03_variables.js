ead77afb6ce7372980a3193a527f0129bb369309
adithyasankars <167020861+adithyasankars@users.noreply.github.com>
2024-04-17 06:31:32 +0000

    Variables

---
+++ JS/03_variables.js
@@ -0,0 +1,32 @@
+
+// Variables are Containers for Storing Data
+
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+// Using const
+
+//------------------------------------------------------------------------------------------ Note-|
+
+
+// The var keyword was used in all JavaScript code from 1995 to 2015.                             |
+
+// The let and const keywords were added to JavaScript in 2015.
+                                                                              
+// The var keyword should only be used in code written for older browsers.                        |
+
+// -----------------------------------------------------------------------------------------------|
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+
+// The two variables price1 and price2 are declared with the const keyword.
+// These are constant values and cannot be changed.
+// The variable total is declared with the let keyword.
+// The value total can be changed.
+
+// -----------------------------------------VARIABLES ARE CONATINERS FOR STORING DATA-------------------------
+
